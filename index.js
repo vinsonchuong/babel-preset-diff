e0a90bcb1038aec7c21a375bfb86448f4bdc1bed
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-27 07:30:40 -0700

    Ensure modules are imported correctly

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const interopRequire = require('interop-require')
+
 module.exports = {
   presets: [
     [
-      require('babel-preset-env'),
+      interopRequire('babel-preset-env'),
       {
         targets: {
           node: 'current'
         }
       }
     ],
-    require('babel-preset-stage-0'),
-    require('babel-preset-flow')
+    interopRequire('babel-preset-stage-0'),
+    interopRequire('babel-preset-flow')
   ],
-  plugins: [require('babel-plugin-package-name-import')]
+  plugins: [interopRequire('babel-plugin-package-name-import')]
 }
